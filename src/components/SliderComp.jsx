41ce5f9ee943707989415af7f7d320eb7c361dff
kiracyagiz <105634620+kiracyagiz@users.noreply.github.com>
2023-08-27 17:39:57 +0200

    m

--- src/components/SliderComp.jsx
+++ src/components/SliderComp.jsx
@@ -18,7 +18,7 @@ const SliderComp = () => {
   return (
     <div>
       <Slider {...settings}>
-        <div className="!flex flex-col md:flex-row justify-between  h-[fit] p-12 bg-purple-200 w-[80vh]">
+        <div className="!flex flex-col md:flex-row justify-between   p-12 bg-pink-200 w-[80vh]">
           <div className="!flex flex-col gap-y-8 p-12 mt-0 md:mt-20">
             <h1 className="text-4xl ">Lorem ipsum dolor sit amet.</h1>
             <p className="max-w-sm md:max-w-lg">
@@ -31,7 +31,7 @@ const SliderComp = () => {
               fugit minima?
             </p>
           </div>
-          <img src={women} className="ml-20 md:ml-0 w-[150px] md:w-[350px] mr-0 md:mr-20 " alt="" />
+          <img src={women} className="ml-20 md:ml-0 w-[150px] md:w-[300px] object-cover mr-0 md:mr-20 " alt="" />
         </div>
         <div className="!flex flex-col md:flex-row justify-between  p-12 bg-blue-200 w-[80vh]">
           <div className="!flex flex-col gap-y-8 p-12 mt-0 md:mt-20">
@@ -46,9 +46,9 @@ const SliderComp = () => {
               fugit minima?
             </p>
           </div>
-          <img src={womensec} className="ml-20 md:ml-0 w-[150px] md:w-[350px] mr-0 md:mr-20 " alt="" />
+          <img src={womensec} className="ml-20 md:ml-0 w-[150px] md:w-[300px] object-cover mr-0 md:mr-20 " alt="" />
         </div>
-        <div className="!flex flex-col md:flex-row justify-between  p-12 bg-pink-200 w-[80vh]">
+        <div className="!flex flex-col md:flex-row justify-between  p-12 bg-purple-200 w-[80vh]">
           <div className="!flex flex-col gap-y-8 p-12 mt-0 md:mt-20">
             <h1 className="text-4xl ">Lorem ipsum dolor sit amet.</h1>
             <p className="max-w-sm md:max-w-lg">
@@ -61,7 +61,7 @@ const SliderComp = () => {
               fugit minima?
             </p>
           </div>
-          <img src={womenthird} className="ml-20 md:ml-0 w-[150px] md:w-[350px] mr-0 md:mr-20 " alt="" />
+          <img src={womenthird} className="ml-20 md:ml-0 w-[150px] md:w-[300px] object-cover mr-0 md:mr-20 " alt="" />
         </div>
       </Slider>
     </div>
