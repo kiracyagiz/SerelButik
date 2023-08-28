01c64cbf8ee5e4f5d9a8b5a8cd620f9ff9098806
kiracyagiz <105634620+kiracyagiz@users.noreply.github.com>
2023-08-28 06:41:09 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import Card from './components/Card';
 import Extra from './components/Extra';
 import Navbar from './components/Navbar';
 import Slider from './components/SliderComp';
-import bikini from '../images/bikini1.jpg'
 import About from './components/About';
-import BestSellers from './components/BestSellers';
 
 const App = () => {
   const settings = {
